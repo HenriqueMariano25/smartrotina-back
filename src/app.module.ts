bbc598843b60193362b62145cd28434a0e05958f
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2025-04-24 23:35:38 -0300

    Adicionar o @nest/config para gerenciar as variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AutenticacaoModule } from './autenticacao/autenticacao.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { ResidenciaModule } from './residencia/residencia.module';
-import { ListaProdutosModule } from './lista-produtos/lista-produtos.module';
-import { RotinaModule } from './rotina/rotina.module';
-import { ProdutoModule } from './produto/produto.module';
+import {Module} from '@nestjs/common';
+import {AutenticacaoModule} from './autenticacao/autenticacao.module';
+import {UsuarioModule} from './usuario/usuario.module';
+import {ResidenciaModule} from './residencia/residencia.module';
+import {ListaProdutosModule} from './lista-produtos/lista-produtos.module';
+import {RotinaModule} from './rotina/rotina.module';
+import {ProdutoModule} from './produto/produto.module';
+import {ConfigModule} from '@nestjs/config';
 
 
 @Module({
-  imports: [AutenticacaoModule, UsuarioModule, ResidenciaModule, ListaProdutosModule, RotinaModule, ProdutoModule],
+  imports: [AutenticacaoModule, UsuarioModule, ResidenciaModule, ListaProdutosModule, RotinaModule, ProdutoModule,
+    ConfigModule.forRoot({isGlobal: true}),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/autenticacao/strategies/jwt.strategy.ts
+++ src/autenticacao/strategies/jwt.strategy.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
-import { AutenticacaoService } from '../autenticacao.service';
+import {Injectable} from '@nestjs/common';
+import {PassportStrategy} from '@nestjs/passport';
+import {Strategy} from 'passport-jwt';
+import {AutenticacaoService} from '../autenticacao.service';
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly autenticacaoService: AutenticacaoService) {
-    const jwtSecret = process.env.JWT_SECRET;
+  constructor(private readonly autenticacaoService: AutenticacaoService,
+              private readonly configService: ConfigService
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET')
     if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined');
     }
